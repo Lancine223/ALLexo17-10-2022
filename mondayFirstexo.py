e6016f72e92abbc72e61bc3e33430e8dd4af5307
Lancine223 <lacinekeita20@gmail.com>
2022-10-18 08:44:37 +0000

    mondayFirstexo.py

--- mondayFirstexo.py
+++ mondayFirstexo.py
@@ -21,7 +21,7 @@ class Table:
         print("Référence est" ,self.reference , "\n la matière est le",self.matiere, ",\n le poids est de ",self.poids, "kg",
     ",\n la hauteur est " ,self.hauteur, "cm,\n la longueur est ",self.longuere,"cm,\n la largère est ",self.largere,"cm, \nle prix fabrique est",self.__prixfab,"F et le nombre de stock est",self.__nbstock)
     def calculer(self):
-        print("\n le resultat de calcule nbstock est : ",self.__prixfab-self.__nbstock)
+        print("\n le resultat de calcule nbstock est : ",self.__prixfab*self.__nbstock)
 t1 = Table("Bko","Bank", 20, 10, 15, 28, 100, 200,10)
 
 t1.affiche()
